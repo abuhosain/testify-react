f286fb387e1c02b8f63da0258e4fb40e757ff754
Hosain <cryptichasan925@gmail.com>
2025-04-15 20:02:53 +0600

    error handling

--- src/__tests__/primaryButton.tsx
+++ src/__tests__/primaryButton.tsx
@@ -1,5 +1,5 @@
-import {render, screen} from "@testing-library/react";
-import PrimaryButton from "./PrimaryButton";
+import {render, screen} from "@testing-library/react"; 
+import PrimaryButton from "../components/PrimaryButton";
 
 describe('PrimaryButton()', () => {
     it('should render courrectly', () => {
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
-    globals: true,                     // Allows using global functions like `describe`, `it`, `expect`
-    environment: 'jsdom',               // Simulates a browser environment
-    setupFiles: ['./src/setupTest.ts']  // Runs this file before your tests start
-  }
-  
-})
+    globals: true, // Allows using global functions like `describe`, `it`, `expect`
+    environment: "jsdom", // Simulates a browser environment
+    setupFiles: ["./src/setupTest.ts"], // Runs this file before your tests start
+    include: ["**/__tests__/**/*.{ts,tsx}", "**/*.{spec,test}.{ts,tsx}"],
+  },
+});
